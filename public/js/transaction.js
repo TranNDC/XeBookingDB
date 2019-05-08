c094ecedca57a3c632433346d8b6253b44dd12b3
1612898 <1612898>
2019-05-08 19:23:11 +0700

    update c4 transaction

---
+++ public/js/transaction.js
@@ -0,0 +1,248 @@
+var res={results:[{
+    "time":"11:11 AM",
+    "transID":"T190317-0004",
+    "userID":"U101",
+    "busID":"B190330-01-01",
+    "phone":"0121.112.121",
+    "email":"abc@kmail.com",
+    "date":"30 Aug 2019",
+    "liscensePlate":"59 V2 65.616",
+    "type":"Sleeper",
+    "departure":"13:30",
+    "arrival":"23:30",
+    "price":"$500",
+    "paymentTime":"11:13 AM, 17 Mar 2019",
+    "fromLocation":"Đà Lạt",
+    "toLocation":"Hồ Chí Minh",
+    "seats":['10B', '11B'],
+    "status":0,//0 == unused , 1 == used,
+    "passenger":[{
+        "name":"John Smith",
+        "gender":"Male",
+        "birthYear":"1991"
+    },{
+        "name":"Marry Smith",
+        "gender":"Female",
+        "birthYear":"1995"
+    }
+    ]
+}, 
+{
+    "time":"10:10 AM",
+    "transID":"T190317-0005",
+    "userID":"U102",
+    "busID":"B190330-01-01",
+    "phone":"0121.112.121",
+    "email":"abc@kmail.com",
+    "date":"30 Jul 2019",
+    "liscensePlate":"59 V2 65.616",
+    "type":"Sleeper",
+    "departure":"13:30",
+    "arrival":"23:30",
+    "price":"$500",
+    "paymentTime":"11:13 AM, 17 Mar 2019",
+    "fromLocation":"Đà Lạt",
+    "toLocation":"Hồ Chí Minh",
+    "seats":['10B', '11A'],
+    "status":0,//0 == unused , 1 == used,
+    "passenger":[{
+        "name":"John Smith",
+        "gender":"Male",
+        "birthYear":"1991"
+    },{
+        "name":"Marry Smith",
+        "gender":"Female",
+        "birthYear":"1995"
+    }
+    ]
+}, 
+{
+    "time":"1:10 AM",
+    "transID":"T190317-0006",
+    "userID":"U103",
+    "busID":"B190330-01-02",
+    "phone":"0121.112.161",
+    "email":"abc@kmail.com",
+    "date":"30 Jun 2019",
+    "liscensePlate":"59 V2 65.516",
+    "type":"Sleeper",
+    "departure":"13:30",
+    "arrival":"23:30",
+    "price":"$500",
+    "paymentTime":"11:13 AM, 17 Mar 2019",
+    "fromLocation":"Đà Lạt",
+    "toLocation":"Hồ Chí Minh",
+    "seats":['10B', '1B'],
+    "status":0,//0 == unused , 1 == used,
+    "passenger":[{
+        "name":"John Smith",
+        "gender":"Male",
+        "birthYear":"1991"
+    },{
+        "name":"Marry Smith",
+        "gender":"Female",
+        "birthYear":"1995"
+    }
+    ]
+}, 
+{
+    "time":"1:16 AM",
+    "transID":"T190317-0007",
+    "userID":"U102",
+    "busID":"B190330-01-01",
+    "phone":"0121.112.121",
+    "email":"abc@kmail.com",
+    "date":"30 May 2019",
+    "liscensePlate":"59 V2 65.616",
+    "type":"Sleeper",
+    "departure":"13:30",
+    "arrival":"23:30",
+    "price":"$500",
+    "paymentTime":"11:13 AM, 17 Mar 2019",
+    "fromLocation":"Cần Thơ",
+    "toLocation":"Hồ Chí Minh",
+    "seats":['1A', '11B'],
+    "status":0,//0 == unused , 1 == used,
+    "passenger":[{
+        "name":"John Smith",
+        "gender":"Male",
+        "birthYear":"1991"
+    },{
+        "name":"Marry Smith",
+        "gender":"Female",
+        "birthYear":"1995"
+    }
+    ]
+}, 
+{
+    "time":"1:15 AM",
+    "transID":"T190317-0008",
+    "userID":"U102",
+    "busID":"B190330-11-01",
+    "phone":"0121.162.121",
+    "email":"abc@kmail.com",
+    "date":"30 April 2019",
+    "liscensePlate":"59 V2 64.616",
+    "type":"Sleeper",
+    "departure":"13:30",
+    "arrival":"23:30",
+    "price":"$500",
+    "paymentTime":"11:13 AM, 17 Mar 2019",
+    "fromLocation":"Đà Lạt",
+    "toLocation":"Cần Thơ",
+    "seats":['10B', '11B'],
+    "status":1,//0 == unused , 1 == used,
+    "passenger":[{
+        "name":"John Smith",
+        "gender":"Male",
+        "birthYear":"1991"
+    },{
+        "name":"Marry Smith",
+        "gender":"Female",
+        "birthYear":"1995"
+    }
+    ]
+},
+, 
+{
+    "time":"1:15 PM",
+    "transID":"T190317-0009",
+    "userID":"U104",
+    "busID":"B190330-12-01",
+    "phone":"0121.162.121",
+    "email":"abc@kmail.com",
+    "date":"30 Mar 2018",
+    "liscensePlate":"59 V2 64.616",
+    "type":"Semi-Seater",
+    "departure":"13:30",
+    "arrival":"23:30",
+    "price":"$500",
+    "paymentTime":"11:13 AM, 17 Mar 2019",
+    "fromLocation":"Hồ Chí Minh",
+    "toLocation":"Cần Thơ",
+    "seats":['1A', '1B'],
+    "status":1,//0 == unused , 1 == used,
+    "passenger":[{
+        "name":"John Smith",
+        "gender":"Male",
+        "birthYear":"1991",
+    },{
+        "name":"Marry Smith",
+        "gender":"Female",
+        "birthYear":"1995"
+    }
+    ]
+},
+{
+    "time":"1:15 PM",
+    "transID":"T190317-0010",
+    "userID":"U102",
+    "busID":"B190330-11-01",
+    "phone":"0121.162.121",
+    "email":"abc@kmail.com",
+    "date":"30 April 2017",
+    "liscensePlate":"59 V2 64.516",
+    "type":"Sleeper",
+    "departure":"13:30",
+    "arrival":"23:30",
+    "price":"$500",
+    "paymentTime":"11:13 AM, 17 Mar 2019",
+    "fromLocation":"Đà Lạt",
+    "toLocation":"Hồ Chí Minh",
+    "seats":['10B', '11B'],
+    "status":1,//0 == unused , 1 == used,
+    "passenger":[{
+        "name":"John Smith",
+        "gender":"Male",
+        "birthYear":"1991"
+    },{
+        "name":"Marry Smith",
+        "gender":"Female",
+        "birthYear":"1995"
+    }
+    ]
+}
+
+]};
+
+Handlebars.registerHelper("inc", function(value, options)
+{
+    return parseInt(value) + 1;
+});
+
+Handlebars.registerHelper("getSeat", function(valueFather, valueSon)
+{
+    let index1 = parseInt(valueFather);
+    let index2 = parseInt(valueSon);
+    return res.results[index1].seats[index2];
+});
+
+$(document).ready(() => {
+
+    let navTemplates = $("#transaction-template").html();
+    let compilednavTemplates = Handlebars.compile(navTemplates);
+    $.ajax("/partials/handlebars-transaction-partial.html").done((navDetail) => {
+        $("body").append(navDetail);
+        Handlebars.registerPartial("transactionPartial", $("#transaction-detail-template").html());
+        $('#transaction-container').html(compilednavTemplates(res));
+        
+        // $('#a2-btn-step-1').click(function(){
+        //     $('#menu1').addClass('active');
+        //     $('#home').removeClass('active');
+        //     $('#menu1').removeClass('fade');
+        //     $('#home').addClass('fade');
+        //     $('#amenu1').addClass('active');
+        //     $('#ahome').removeClass('active');
+        //   });
+        //   $('#a2-btn-step-2').click(function(){
+        //     $('#menu2').addClass('active');
+        //     $('#menu1').removeClass('active');
+        //     $('#menu2').removeClass('fade');
+        //     $('#menu1').addClass('fade');
+        //     $('#amenu2').addClass('active');
+        //     $('#amenu1').removeClass('active');
+        //   });
+    });
+
+
+})
