c046160e2ecba62827acf924e9ee2ea5e7f3d934
1612842 <1612842@student.hcmus.edu.vn>
2019-06-07 01:21:20 +0700

    update login, sign up

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 var express = require('express');
 var app = express();
 
-// Setting for app here
-
 // Set Public Folder
 app.use(express.static(__dirname + '/public'));
 
 // Use View Engine
 var expressHbs = require('express-handlebars');
 var hbs = expressHbs.create({
-	extname			: 'hbs',
-	defaultLayout	: 'layout', 
-	layoutsDir		: __dirname + '/views/layouts/',
-	partialsDir		: __dirname + '/views/partials/',
+	extname: 'hbs',
+	defaultLayout: 'layout',
+	layoutsDir: __dirname + '/views/layouts/',
+	partialsDir: __dirname + '/views/partials/',
 });
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
@@ -22,56 +20,83 @@ app.set('port', (process.env.PORT || 5000));
 
 // Create database
 var models = require('./models');
-app.get('/sync', function(req, res){
-	models.sequelize.sync().then(function(){
+app.get('/sync', function (req, res) {
+	models.sequelize.sync().then(function () {
 		res.send('database sync completed!');
 	});
 });
 
+//use body parser
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
 
 
-// Define your route here
-// Test view render
+// use session
+var session = require('express-session');
+app.use(session({
+	cookie: {
+		httpOnly: true,
+		maxAge: 30 * 24 * 60 * 60 * 1000
+	}, //30 days
+	secret: 'Secret',
+	saveUninitialized: false,
+	resave: false
+}));
+
+//use express validator
+var expressValidator = require('express-validator');
+app.use(expressValidator());
+
+app.use(function (req, res, next) {
+	res.locals.user = req.session.user;
+	res.locals.isLoggedIn = req.session.user ? true : false;
+	next();
+});
 
 var indexRouter = require('./routes/index');
-app.use('/',indexRouter);
+app.use('/', indexRouter);
 
-app.get('/login', function(req, res){
+var userRouter = require('./routes/users');
+app.use('/users', userRouter);
+
+app.get('/login', function (req, res) {
 	res.render('login');
 });
 
-app.get('/admin', function(req, res){
+app.get('/admin', function (req, res) {
 	res.render('admin');
 });
 
-app.get('/detail', function(req, res){
+app.get('/detail', function (req, res) {
 	res.render('detail');
 });
 
-app.get('/contact', function(req, res){
+app.get('/contact', function (req, res) {
 	res.render('contact');
 });
 
-app.get('/signup', function(req, res){
+app.get('/signup', function (req, res) {
 	res.render('signup');
 });
 
-app.get('/masterdata', function(req, res){
+app.get('/masterdata', function (req, res) {
 	res.render('masterdata');
 });
 
-app.get('/transaction', function(req, res){
+app.get('/transaction', function (req, res) {
 	res.render('transaction');
 });
 
-app.get('/profile', function(req, res){
-	user={
-		name:"Lê Thành Công",
-		avatarlink:"/img/user/user.jpg"
+app.get('/profile', function (req, res) {
+	user = {
+		name: "Lê Thành Công",
+		avatarlink: "/img/user/user.jpg"
 	}
-	res.render('profile',user);
+	res.render('profile', user);
 });
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function () {
 	console.log('Server is listening at port ' + app.get('port'));
 });
---
+++ routes/users.js
@@ -0,0 +1,86 @@
+var express = require('express');
+var router = express.Router();
+
+var userController = require('../controllers/users');
+
+router.get('/login', (req, res) => {
+    req.session.returnURL = req.query.returnURL;
+    res.render('login',{noHeader:"true",noFooter:"true"});
+});
+
+router.get('/signup', (req, res) => {
+    res.render('signup',{noHeader:"true",noFooter:"true"});
+});
+
+router.get('/profile', userController.isLoggedIn, (req, res) => {
+    res.render('profile');
+});
+
+router.get('/admin', userController.isAdmin, (req, res) => {
+    res.render('admin');
+});
+
+router.post('/login', function(req, res) {
+    var email = req.body.email;
+    var password = req.body.password;
+
+    userController.getUserByEmail(email, function(user) {
+        if (!user) {
+            res.render('login', { error: 'No email is found' });
+        } else {
+            userController.comparePassword(password, user.password, function(isMatch) {
+                if (!isMatch) {
+                    res.render('login', { error: 'Incorrect Password' });
+                } else {
+                    req.session.user = user;
+                    if (req.session.returnURL) {
+                        res.redirect(req.session.returnURL);
+                    } else {
+                        if (user.isAdmin === true) {
+                            res.redirect('/users/admin');
+                        } else {
+                            res.redirect('/');
+                        }
+                    }
+                }
+            });
+        }
+    });
+});
+
+router.post('/signup', function(req, res) {
+    var name = req.body.name;
+    var email = req.body.email;
+    var password = req.body.password;
+
+    req.checkBody('email', 'Please enter a valid email').isEmail();
+    
+    var errors = req.validationErrors();
+    if (errors) {
+        res.render('signup', { errors: errors });
+    } else {
+        userController.getUserByEmail(email, function(user) {
+            if (user) {
+                res.render('signup', { error: `Email ${email} exists! Please choose another email.` });
+            } else {
+                var user = {
+                    name: name,
+                    email: email,
+                    password: password,
+                    isAdmin: false
+                };
+                userController.createUser(user, function(err) {
+                    if (err) throw err;
+                    res.render('login', { success: 'You have registered, now please login' });
+                });
+            }
+        });
+    }
+});
+
+router.get('/logout', function(req, res) {
+    req.session.user = null;
+    res.redirect('/users/login');
+});
+
+module.exports = router;
